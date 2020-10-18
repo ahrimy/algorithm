7dd68cc372c8289463f595d7f256adf210d02b79
ahrimy <dlsdjdkfla@gmail.com>
2020-10-18 19:08:50 +0900

    ✏️ 가운데 글자 가져오기

--- practice/CenterLetter.java
+++ practice/CenterLetter.java
@@ -1,17 +1,17 @@
-package practice;
 /*
  * 연습문제
  * 가운데 글자 가져오기
  * Java
  */
-public class CenterLetter {
-	  public String solution(String s) {
-	      int length = s.length();
-	      if(length % 2 == 0){
-	            s = s.substring(length/2-1,length/2+1);
-	        } else {
-	            s = s.substring(length/2, length/2+1);
-	        }
-	      return s;
-	  }
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int length = s.length();
+        if(length%2==0){
+            answer = s.substring(length/2-1,length/2+1);
+        }else{
+            answer = s.substring(length/2,length/2+1);
+        }
+        return answer;
+    }
 }
