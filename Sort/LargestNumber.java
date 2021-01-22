3b448ec2b25b18a95b5720d600b4fefdb24a4422
Ahrim Yang <dlsdjdkfla@gmail.com>
2021-01-22 18:45:09 +0900

    ✏️ 가장 큰 수

---
+++ Sort/LargestNumber.java
@@ -0,0 +1,39 @@
+
+/**
+ * 정렬
+ * 가장 큰 수
+ * Java
+ */
+
+
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        
+        String[] arr = new String[numbers.length];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(arr, new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+
+				return ((o2 + o1).compareTo(o1 + o2));
+			}
+		});
+        
+		if(arr[0].equals("0")) {
+			return "0";
+		}
+		
+		for (String a : arr) {
+			answer += a;
+		}
+        
+        return answer;
+    }
+}
