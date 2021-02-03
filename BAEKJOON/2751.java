8acf77cc84e3e89d8927933cade39f217a1b8468
ahrimy <dlsdjdkfla@gmail.com>
2021-02-03 20:08:51 +0900

    ✏️ 수 정렬하기

---
+++ BAEKJOON/2751.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            arr.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(arr);
+
+        for(int value: arr){
+            bw.write(value+"\n");
+        }
+        bw.close();
+    }
+}
+
+/**
+ Scanner, System.out.println 사용하면 시간초과
+ */
