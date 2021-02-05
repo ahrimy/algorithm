097c51f769194bfbb9da926634b11aa5bef92c97
ahrimy <dlsdjdkfla@gmail.com>
2021-02-05 15:25:32 +0900

    ✏️ 수 정렬하기3

---
+++ BAEKJOON/10989.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+class Main{
+    public final static int MAX_NUM = 10000;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        arr = sort(arr);
+
+        for(int value: arr){
+            bw.write(value+"\n");
+        }
+        bw.close();
+    }
+    public static int[] sort(int[] arr){
+        int[] temp = new int[arr.length];
+        int[] count = new int[MAX_NUM+1];
+        for(int value:arr){
+            count[value]++;
+        }
+        for(int i=1;i<count.length;i++){
+            count[i] += count[i-1];
+        }
+        for(int value:arr){
+            temp[--count[value]] = value;
+        }
+        return temp;
+    }
+}
