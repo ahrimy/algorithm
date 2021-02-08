f9b713b68270312b54163e5b8042016b023b61ed
ahrimy <dlsdjdkfla@gmail.com>
2021-02-09 00:42:05 +0900

    ✏️ 수 정렬하기 2

--- BAEKJOON/정렬/2751.java
+++ BAEKJOON/정렬/2751.java
@@ -5,24 +5,24 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
 
-class Main{
-    public static void main(String[] args) throws IOException{
+class Sort2751 {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         ArrayList<Integer> arr = new ArrayList<>();
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             arr.add(Integer.parseInt(br.readLine()));
         }
         Collections.sort(arr);
 
-        for(int value: arr){
-            bw.write(value+"\n");
+        for (int value : arr) {
+            bw.write(value + "\n");
         }
         bw.close();
     }
 }
 
 /**
- Scanner, System.out.println 사용하면 시간초과
+ * Scanner, System.out.println 사용하면 시간초과
  */
