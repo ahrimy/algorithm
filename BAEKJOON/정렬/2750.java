92ddc80217483273555e6103bf4fec15250a5b16
ahrimy <dlsdjdkfla@gmail.com>
2021-02-09 00:42:31 +0900

    ✏️ 수 정렬하기

--- BAEKJOON/정렬/2750.java
+++ BAEKJOON/정렬/2750.java
@@ -1,16 +1,17 @@
 import java.util.*;
 
-class Main{
-    public static void main(String[] args){
+class Sort2750 {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int[] arr = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=scan.nextInt();
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
         }
         Arrays.sort(arr);
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             System.out.println(arr[i]);
         }
+        scan.close();
     }
 }
