90602721a74a02d991da2f90fd1f7be8fa3cb697
ahrimy <dlsdjdkfla@gmail.com>
2021-02-09 00:43:30 +0900

    ✏️ 소트인사이드

--- BAEKJOON/정렬/1427.java
+++ BAEKJOON/정렬/1427.java
@@ -1,13 +1,13 @@
-import java.util.*;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
 
-class Main{
+class Sort1427 {
     public final static int MAX_NUM = 9;
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String str = br.readLine();
@@ -16,17 +16,18 @@ class Main{
         bw.write(answer + "\n");
         bw.close();
     }
-    public static String sort(String str){
+
+    public static String sort(String str) {
         String temp = "";
-        int[] count = new int[MAX_NUM+1];
-        for(int i=0;i<str.length();i++){
-            int value = Integer.parseInt(str.substring(i,i+1));
+        int[] count = new int[MAX_NUM + 1];
+        for (int i = 0; i < str.length(); i++) {
+            int value = Integer.parseInt(str.substring(i, i + 1));
             count[value]++;
         }
         int index = 0;
         int countIdx = 0;
-        while(index<str.length()){
-            if(count[countIdx]==0){
+        while (index < str.length()) {
+            if (count[countIdx] == 0) {
                 countIdx++;
                 continue;
             }
