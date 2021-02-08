3b4624e64e0e624709c16f42e2b504cc75cb0ff2
ahrimy <dlsdjdkfla@gmail.com>
2021-02-09 00:44:19 +0900

    ✏️ 좌표 정렬하기

--- BAEKJOON/정렬/11650.java
+++ BAEKJOON/정렬/11650.java
@@ -5,22 +5,22 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
 
-class Main{
-    public static void main(String[] args) throws IOException{
+class Sort11650 {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         ArrayList<Point> list = new ArrayList<>();
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             String[] temp = br.readLine().split(" ");
-            list.add(new Point(Integer.parseInt(temp[0]),Integer.parseInt(temp[1])));
+            list.add(new Point(Integer.parseInt(temp[0]), Integer.parseInt(temp[1])));
         }
 
         Collections.sort(list);
 
-        for(int i=0;i<list.size();i++){
+        for (int i = 0; i < list.size(); i++) {
             Point p = list.get(i);
-            bw.write(p.x+" "+p.y+"\n");
+            bw.write(p.x + " " + p.y + "\n");
         }
         bw.close();
     }
@@ -28,18 +28,18 @@ class Main{
 
 class Point implements Comparable<Point> {
     int x, y;
-    public Point(int x, int y){
+
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
-    
+
     @Override
     public int compareTo(Point p) {
-        if(this.x > p.x) {
+        if (this.x > p.x) {
             return 1; // x에 대해서는 오름차순
-        }
-        else if(this.x == p.x) {
-            if(this.y > p.y) { // y에 대해서는 내림차순
+        } else if (this.x == p.x) {
+            if (this.y > p.y) { // y에 대해서는 내림차순
                 return 1;
             }
         }
