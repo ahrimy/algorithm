e4ed226c5d5a7efd096f91b8f32cfb8334a4e9f5
ahrimy <dlsdjdkfla@gmail.com>
2021-03-03 20:39:14 +0900

    ✏️ 정수 삼각형

---
+++ BAEKJOON/DP/1932.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+class _1932 {
+    public static int[][] dp;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[n+1][n+1];
+        for(int i=1;i <= n;i++){
+            String[] temp = br.readLine().split(" ");
+            for(int j=1;j<=i;j++){
+                if(j == 1){
+                    dp[i][j] = dp[i-1][j] + Integer.parseInt(temp[j-1]);
+                }else if(j == i){
+                    dp[i][j] = dp[i-1][j-1] + Integer.parseInt(temp[j-1]);
+                }else{
+                    dp[i][j] = (dp[i-1][j]>dp[i-1][j-1]?dp[i-1][j]:dp[i-1][j-1]) + Integer.parseInt(temp[j-1]);
+                }
+            }
+        }
+        
+        int answer = 0; 
+        for(int i=1; i<=n; i++){
+            if(answer < dp[n][i]){
+                answer = dp[n][i];
+            }
+        }
+        bw.write(answer + "\n");
+        bw.close();
+    }
+}
