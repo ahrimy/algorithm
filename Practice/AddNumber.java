6631086be2441adb21cd5bcdc312d05261a944af
ahrimy <dlsdjdkfla@gmail.com>
2021-09-14 23:44:51 +0900

    ✏️ 자릿수 더하기

---
+++ Practice/AddNumber.java
@@ -0,0 +1,18 @@
+/**
+ * 연습문제
+ * 자릿수 더하기
+ * Java
+ */
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        while(n > 0){
+            answer += n%10;
+            n = n/10;
+        }
+        
+        return answer;
+    }
+}
