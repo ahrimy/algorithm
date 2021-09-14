22b2e87053bc1822ef3995448ff450a41ba10e1b
ahrimy <dlsdjdkfla@gmail.com>
2021-09-14 23:50:12 +0900

    ✏️ 정수 내림차순으로 배치하기

---
+++ Practice/IntegerSortDesc.java
@@ -0,0 +1,26 @@
+/**
+ * 연습문제
+ * 정수 내림차순으로 배치하기
+ * Java
+ */
+
+import java.util.Arrays;
+import java.util.Collections;
+
+class IntegerSortDesc {
+    public long solution(long n) {
+        long answer = 0;
+        int length = (int)(Math.log10(n)+1);
+        Integer[] number = new Integer[length];
+        for(int i=0;i<length;i++){
+            number[i] = (int)(n%10);
+            n = n/10;
+        }
+        Arrays.sort(number,Collections.reverseOrder());
+        for(int i=0;i<length;i++){
+            answer *= 10;
+            answer += number[i];
+        }
+        return answer;
+    }
+}
