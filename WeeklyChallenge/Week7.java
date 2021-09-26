9867d6a8b2709e23cd8193af44378200ded3cbea
ahrimy <dlsdjdkfla@gmail.com>
2021-09-26 19:46:31 +0900

    ✏️ 7주차

--- WeeklyChallenge/Week7.java
+++ WeeklyChallenge/Week7.java
@@ -4,30 +4,23 @@
  * Java
  */
 
-import java.util.Queue;
-import java.util.LinkedList;
 import java.util.HashSet;
 
 public class Week7 {
     public int[] solution(int[] enter, int[] leave) {
         int[] answer = new int[enter.length];
-        Queue<Integer> enterQueue = new LinkedList<>();
-        Queue<Integer> leaveQueue = new LinkedList<>();
-        for(int i=0;i<enter.length;i++) {
-            enterQueue.add(enter[i]);
-            leaveQueue.add(leave[i]);
-        }
+        int enterIndex = 0;
+        int leaveIndex = 0;
         HashSet<Integer> set = new HashSet<>();
-        set.add(enterQueue.poll());
-        while(!enterQueue.isEmpty()) {
-            while(leaveQueue.peek()!=null && set.contains(leaveQueue.peek())) {
-                set.remove(leaveQueue.poll());
+        while(enterIndex < enter.length) {
+            while(leaveIndex < leave.length && set.contains(leave[leaveIndex])) {
+                set.remove(leave[leaveIndex++]);
             }
             for(int num: set) {
                 answer[num-1]++;
             }
-            answer[enterQueue.peek()-1] = set.size();
-            set.add(enterQueue.poll());
+            answer[enter[enterIndex]-1] = set.size();
+            set.add(enter[enterIndex++]);
         }
         return answer;
     }
