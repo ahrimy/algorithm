14f0628d28bb739583ac12a8d4976f71abc3b8f7
ahrimy <dlsdjdkfla@gmail.com>
2021-10-06 23:42:01 +0900

    ✏️ 땅따먹기

---
+++ Practice/Hopscotch.java
@@ -0,0 +1,23 @@
+/**
+ * 연습문제
+ * 땅따먹기
+ * Java
+ */
+
+public class Hopscotch {
+    public int solution(int[][] land) {    
+        // 재귀로 풀면 효율성 실패....
+        int answer = 0;
+        for(int i=1;i<land.length;i++) {
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][2]));
+        }
+        for(int i=0;i<4;i++) {
+            if(answer < land[land.length-1][i]) answer = land[land.length-1][i];   
+        }
+        
+        return answer;
+    }
+}
