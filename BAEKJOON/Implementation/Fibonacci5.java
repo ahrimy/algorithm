9a3a5553e14d6422bb5a39969ace766611d9c1c5
ahrimy <dlsdjdkfla@gmail.com>
2021-10-08 01:38:56 +0900

    ✏️ 피보나치 수 5

---
+++ BAEKJOON/Implementation/Fibonacci5.java
@@ -0,0 +1,23 @@
+/**
+ * 10870
+ * 피보나치 수 5
+ * Java
+ */
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int answer = fibonacci(n);
+        System.out.println(answer);
+        scan.close();
+    }
+
+    public static int fibonacci(int n) {
+        if(n < 2) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
