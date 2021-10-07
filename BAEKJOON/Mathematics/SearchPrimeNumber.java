ca81cc66d50851003971dc2756dfec7fe8951214
ahrimy <dlsdjdkfla@gmail.com>
2021-10-08 01:39:43 +0900

    ✏️ 소수 찾기

---
+++ BAEKJOON/Mathematics/SearchPrimeNumber.java
@@ -0,0 +1,33 @@
+/**
+ * 1978
+ * 소수 찾기
+ * Java
+ */
+
+import java.util.Scanner;
+
+public class SearchPrimeNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        
+        int count = 0;
+        for(int i=0;i<n;i++) {
+            if(isPrimeNumber(scan.nextInt())) count++;
+        }
+        System.out.println(count);
+        scan.close();
+    }
+    public static boolean isPrimeNumber(int n) {
+        boolean isPrime = true;
+        if(n == 1) return false;
+        for(int i=2;i*i<=n;i++) {
+            if(n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }   
+}
